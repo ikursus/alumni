98db4b5ab09935999d9031b4f9faa4d150662a5f
Amirol Zolkifli <usahaniaga.com@gmail.com>
2019-10-01 15:23:39 +0800

    Migrations files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedInteger('title_id');
             $table->string('name');
+            $table->string('nric');
+            $table->string('gender');
+            $table->string('phone');
+            $table->text('address');
+            $table->unsignedInteger('race_id')->nullable();
+            $table->unsignedInteger('religion_id')->nullable();
+            $table->unsignedInteger('nationality_id')->nullable();
+            $table->string('gambar')->nullable();
+            $table->string('status_perkahwinan');
+            $table->string('role');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
